de15ae8e86d933c9706aeda9ed22155ab285b566
Edgar HIPP <edgar.hipp@netapsys.fr>
2015-04-28 09:41:22 +0200

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ _ = require('lodash');
 
 var argv = require('minimist')(process.argv.slice(2));
 
-defaults = {milliseconds:1000};
+defaults = {milliseconds:1000,queue:false};
 
 argv = _.merge(argv,defaults)
 
@@ -13,21 +13,27 @@ process.stdin.setEncoding('utf8');
 
 var lingeringLine = "";
 var lastTime=0;
+var lines = [];
 
 var processLine = function(line){
     var currentTime = new Date();
-    if (+lastTime+argv.milliseconds<+currentTime)
-    {
+    if (+lastTime+argv.milliseconds<+currentTime) {
         lastTime=new Date();
-        return console.log(line);
+        console.log(line);
+        return true;
     }
+    return false;
 };
 
 process.stdin.on('data', function(chunk) {
-    var lines = chunk.split("\n");
+    if(argv.queue===false)
+        lines = []
+    lines.concat(chunk.split("\n"));
     lines[0] = lingeringLine + lines[0];
     lingeringLine = lines.pop();
-    lines.forEach(processLine);
+    lines.forEach(function(line){
+        processLine(line);
+    });
 });
 
 
